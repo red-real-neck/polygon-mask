3adcc3dd979d914a36618bfe3ad2e6399d4b8ca1
Ruslan <faleot.karas@gmail.com>
2022-08-19 10:36:20 +0300

    dynamic canvas w/h above img

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
-const canvas = document.querySelector("#viewport");
-const context = canvas.getContext('2d');
+const img = document.querySelector(".img");
+const width = img.clientWidth;
+const height = img.clientHeight;
+const canvas = document.createElement("canvas");
 
-const make_base = () => {
-    base_image = new Image();
-    base_image.src = 'images/16-08-2022-18_54_47-006202MB.jpg';
-    base_image.onload = function(){
-      context.drawImage(base_image, 0, 0);
-    }
-  }
-  
-  make_base();
+canvas.classList.add("canvas")
+canvas.width = width;
+canvas.height = height;
 
-var s = Snap("#svg");
+img.parentNode.insertBefore(canvas, img.nextSibling);
 
-var circle = s.circle(90,120,80);
-var square = s.rect(210,40,160,160);
-var ellipse = s.ellipse(460,120,50,80);
+const ctx = canvas.getContext("2d");
+ctx.strokeRect(75, 140, 150, 110);
