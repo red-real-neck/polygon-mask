d50b8746754a57eaa7aa3c4727f89d26b1b98fe8
Ruslan <faleot.karas@gmail.com>
2022-08-23 00:10:28 +0300

    almost done

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ const width = img.clientWidth;
 const height = img.clientHeight;
 const canvas = document.createElement("canvas");
 const UICanvas = document.createElement("canvas");
+const radius = 7;
+const eventCondition = {
+  mouseDown: false,
+  mouseMove: false,
+}
+let currentVertex = null;
 
 canvas.classList.add("canvas");
 canvas.width = width;
@@ -24,29 +30,59 @@ offsetX = BB.left;
 offsetY = BB.top;
 
 const coordinates = [];
-var isDone = false;
 
 ctx.lineWidth = 2;
 ctx.strokeStyle = "blue";
+ctx.fillStyle = "rgba(255, 255, 0, 0.25)";
 
 UICtx.fillStyle = "red"
 
 const handleMouseDown = (event) => {
-  if (isDone || coordinates.length > 10) return;
+  if (coordinates.length > 10) return;
 
+  
   event.preventDefault();
   event.stopPropagation();
-
+  
   mouseX = parseInt(event.clientX - offsetX);
   mouseY = parseInt(event.clientY - offsetY);
+
+  if (intersectedVertex({x: mouseX, y: mouseY}) !== false) {
+    currentVertex = intersectedVertex({x: mouseX, y: mouseY});
+    return;
+  }
   coordinates.push([mouseX, mouseY]);
   drawPolygon();
   drawCircles();
 };
 
-canvas.addEventListener("click", (e) => {
+canvas.addEventListener("mousedown", (e) => {
+  eventCondition.mouseDown = true;
   handleMouseDown(e);
+  console.log('coordinates:', coordinates);
 });
+
+canvas.addEventListener("mousemove", (e) => {
+  if (eventCondition.mouseDown) {
+    handleMouseMove(e);
+  } else return;
+})
+
+canvas.addEventListener("mouseup", (e) => {
+  eventCondition.mouseDown = false;
+  currentVertex = null;
+  }
+)
+
+function handleMouseMove(e) {
+  mouseX = parseInt(e.clientX - offsetX);
+  mouseY = parseInt(e.clientY - offsetY);
+  ctx.clearRect(0, 0, width, height);
+  UICtx.clearRect(0, 0, width, height);
+  coordinates[currentVertex] = [mouseX, mouseY];
+  drawPolygon();
+  drawCircles();
+}
 
 function drawPolygon() {
   ctx.clearRect(0, 0, width, height);
@@ -57,15 +93,25 @@ function drawPolygon() {
   }
   ctx.closePath();
   ctx.stroke();
-  console.log('coordinates:', coordinates);
+  ctx.fill();
 }
 
 function drawCircles() {
+  UICtx.clearRect(0, 0, width, height);
   for (index = 0; index < coordinates.length; index++) {
     UICtx.moveTo(coordinates[index][0], coordinates[index][1]);
-    UICtx.arc(coordinates[index][0], coordinates[index][1], 7, 0, 2 * Math.PI, false);
+    UICtx.beginPath(coordinates[index][0], coordinates[index][1]);
+    UICtx.arc(coordinates[index][0], coordinates[index][1], radius, 0, 2 * Math.PI, false);
     UICtx.fill();
   }
-  // UICtx.closePath();
-  // UICtx.stroke();
+}
+
+function intersectedVertex(mouseCoords) {
+  for (let index = 0; index < coordinates.length; index++) {
+    const d = Math.sqrt((mouseCoords.x - coordinates[index][0]) * (mouseCoords.x - coordinates[index][0]) + (mouseCoords.y - coordinates[index][1]) * (mouseCoords.y - coordinates[index][1]));
+    if (d <= radius - radius || d < radius + radius || d == radius + radius) {
+        return index;
+    }
+  }
+  return false;
 }
